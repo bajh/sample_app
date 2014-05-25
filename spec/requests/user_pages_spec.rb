9c1ecec22f760da4a088036af64c33fcaf989f53
Brandon hopkins <brhpkns@gmail.com>
2014-05-25 11:37:41 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,11 +21,13 @@ describe "UserPages" do
 	describe "signup" do
 		before { visit signup_path }
 		let(:submit) { "Create my account" }
+
 		describe "with invalid information" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
 		end
+
 		describe "with valid information" do
 			before do
 				fill_in "Name", with: "Example User"
@@ -35,6 +37,19 @@ describe "UserPages" do
 			end
 			it "should create a user" do
 				expect { click_button submit}.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit}
+				it {should have_link('Sign out', href: signout_path)}
+			end
+
+			describe "followed by signout" do
+				before do 
+					click_button submit
+					click_link "Sign out"
+				end
+				it { should have_link('Sign in')}
 			end
 		end
 	end
