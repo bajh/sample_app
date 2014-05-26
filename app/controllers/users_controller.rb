1536264e2c9d8414410f934b19b2fae2f4f3cb46
Brandon hopkins <brhpkns@gmail.com>
2014-05-26 15:49:22 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
 
   def new
   	@user = User.new
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -26,6 +27,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
   end
 
   def update
@@ -50,4 +57,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Michael Hartl"
-		email "Michael@example.com"
+		sequence(:name) {|n| "Person #{n}"}
+		sequence(:email) {|n| "person#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
+		factory :admin do
+			admin true
+		end
 	end
 end
